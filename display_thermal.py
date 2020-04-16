89ad4db2df06f4d3d9abc61f0bee90e8030c6cd3
Jeremy Bobbin <jer@jer.cxc>
2020-04-16 15:55:29 -0700

    normalize

--- display_thermal.py
+++ display_thermal.py
@@ -18,6 +18,7 @@ def main():
 
 	t = []
 	throughput = []
+	start = 0
 	
 	# second, throughput, temp[, temp[, temp...]]
 	with open(file_name) as f:
@@ -33,8 +34,10 @@ def main():
 	with open(file_name) as f:
 		for line in f:
 			iterms = line.split(', ')
+			if start is 0:
+				start = int(iterms[0])
 			print(iterms)
-			t.append(int(iterms[0]))
+			t.append(int(iterms[0]) - start)
 			throughput.append(float(iterms[1]))
 			for i in range(2, len(iterms) - 1):
 				temperature[i-2].append(int(iterms[i]))
