fdd0345dea24ff285c18717f5bf9e96eb74bc57e
chuanli11 <cl.chuanli@gmail.com>
2020-06-18 23:36:37 -0700

    Set batch size differently for fp32 and fp16

--- config_resnet50_replicated_fp32_train_syn.sh
+++ config_resnet50_replicated_fp32_train_syn.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
 MODELS="resnet50"
 VARIABLE_UPDATE="replicated"
-PRECISION="fp32"
+PRECISION="fp32 fp16"
 RUN_MODE="train"
 DATA_MODE="syn"
 
-case "${GPU_RAM:-'12GB'}" in
-	'6GB') 
-		resnet50=32
-		;;
-	'8GB')
-		resnet50=48
-		;;
-	'12GB')
-		resnet50=64
-		;;
-	'16GB')
-		resnet50=96
-		;;		
-	'23GB'|'24GB')
-		resnet50=128
-		;;
-	'31GB'|'32GB')
-		resnet50=192
-		;;
-	'47GB'|'48GB')
-		resnet50=256
-		;;
-	*) echo "Batchsize for VRAM size '$GPU_RAM' not optimized" >&2;;
-esac
