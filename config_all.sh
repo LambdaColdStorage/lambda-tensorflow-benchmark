526db601d1e3d1c09ab2ea5375bd893c0c27392e
chuanli11 <cl.chuanli@gmail.com>
2020-06-19 07:46:00 -0700

    remove ssd300

--- config_all.sh
+++ config_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MODELS="inception3 resnet152 resnet50 alexnet inception4 vgg16 ssd300"
+MODELS="inception3 resnet152 resnet50 alexnet inception4 vgg16"
 VARIABLE_UPDATE="replicated parameter_server"
 PRECISION="fp32 fp16"
 RUN_MODE="train inference"
