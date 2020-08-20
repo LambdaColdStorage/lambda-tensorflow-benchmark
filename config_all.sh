848dad9ca3b677a4cf4468c65917183f3f051f88
chuanli11 <cl.chuanli@gmail.com>
2020-08-20 08:41:17 -0700

    Update config_all.sh

--- config_all.sh
+++ config_all.sh
@@ -3,5 +3,5 @@ MODELS="inception3 resnet152 resnet50 alexnet inception4 vgg16"
 VARIABLE_UPDATE="replicated"
 PRECISION="fp16 fp32"
 RUN_MODE="train inference"
-DATA_MODE="real syn"
+DATA_MODE="syn"
 
